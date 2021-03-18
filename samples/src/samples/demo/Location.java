5dc3d172aa8ad310c8035c54a20b643b7cfaa42f
Fatin Najiha <fnajiha01@gmail.com>
2021-03-18 20:56:21 +0800

    Initial Commit 2

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,16 @@
+package samples.demo;
+
+public class Location {
+
+	private String x;
+	private int locationId;
+	private int description;
+	
+	public String getX() {
+		return this.x;
+	}
+	
+	public void setX(String name) {
+		this.x = name;
+	}
+}
